5d35cb649987a4678d196806fbd181fece471a29
om-kanthariya <omkanthariya04@gmail.com>
2025-06-23 22:59:53 +0530

    UI AND API Code setUp

---
+++ API_fileCompression/src/main/java/com/backend/API_fileCompression/ApiFileCompressionApplication.java
@@ -0,0 +1,13 @@
+package com.backend.API_fileCompression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFileCompressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFileCompressionApplication.class, args);
+	}
+
+}
---
+++ API_fileCompression/src/test/java/com/backend/API_fileCompression/ApiFileCompressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.API_fileCompression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFileCompressionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
